bdfd2fcb122b32e310b4b7a19163f0f098b36a04
Elizeu Santos <elyzeu.tec@gmail.com>
2025-08-22 14:02:32 -0300

    Add ResumeItem component and styles; update Resume component to use ResumeItem

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,8 +1,15 @@
 import React from "react";
+import ResumeItem from "../ResumeItem";
 import * as C from "./styles";
 
 const Resume = () => {
-  return <C.Container>Resume component</C.Container>
+  return (
+    <C.Container>
+      <ResumeItem />
+      <ResumeItem />
+      <ResumeItem />
+    </C.Container>
+  );
 };
 
 export default Resume;
