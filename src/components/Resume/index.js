4402ce53c8a00d4fcdb8e512f148d1556041e992
Elizeu Santos <elyzeu.tec@gmail.com>
2025-08-25 12:08:10 -0300

    Refactor Resume and ResumeItem components to accept props for title, icon, and value

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ResumeItem from "../ResumeItem";
 import * as C from "./styles";
+import {
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign,
+} from "react-icons/fa";
 
 const Resume = () => {
   return (
     <C.Container>
-      <ResumeItem />
-      <ResumeItem />
-      <ResumeItem />
+      <ResumeItem title="Entradas" Icon={FaRegArrowAltCircleUp} value={3000} />
+      <ResumeItem title="Saídas" Icon={FaRegArrowAltCircleDown} value={1600} />
+      <ResumeItem title="Total" Icon={FaDollarSign} value={1400} />
     </C.Container>
   );
 };
